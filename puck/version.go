9503ed3b64c66f07cad95bfad8e1346f3da23087
DeedleFake <deedlefake@users.noreply.github.com>
2017-02-28 20:15:10 -0500

    puck: Switch to far simpler semver-based versioning scheme.

--- puck/version.go
+++ puck/version.go
@@ -2,120 +2,73 @@ package puck
 
 import (
 	"bytes"
-	"fmt"
 	"strconv"
-	"strings"
 )
 
-// A Version holds information about a package's version. It is based
-// on Arch Linux's pacman's versioning model. Unlike pacman, however,
-// package release numbers start at 0, not 1.
+// A Version holds information about a package's version. It is
+// loosely based on Arch Linux's pacman's versioning model. There are
+// a few significant differences, however:
+//
+// * The package version section of the version is based on semver,
+//   rather than Arch's complicated alphanumeric versioning scheme.
+//   Unlike semver, however, it allows as many sections to the version
+//   as are necessary. This is to allow versions to be devised that
+//   coorespond more appropriately with upstream version numbers.
+// * Package release numbers start at 0, not 1.
 type Version struct {
-	Epoch uint
-	Ver   string
-	Rel   uint
+	Epoch int
+	Ver   []int
+	Rel   int
 }
 
 func (ver Version) String() string {
 	var buf bytes.Buffer
 
 	if ver.Epoch != 0 {
-		fmt.Fprintf(&buf, "%v:", ver.Epoch)
+		buf.WriteString(strconv.FormatInt(int64(ver.Epoch), 10))
 	}
-	fmt.Fprintf(&buf, "%v-%v", ver.Ver, ver.Rel)
+
+	buf.WriteRune('v')
+	buf.WriteString(strconv.FormatInt(int64(ver.Ver[0]), 10))
+	for _, v := range ver.Ver[1:] {
+		buf.WriteRune('.')
+		buf.WriteString(strconv.FormatInt(int64(v), 10))
+	}
+
+	buf.WriteRune('-')
+	buf.WriteString(strconv.FormatInt(int64(ver.Rel), 10))
 
 	return buf.String()
 }
 
-// These constants designate which part of a version a version
-// comparison was decided by and what the outcome was. They are
-// centered around 0 so that they can be ignored if their not
-// necessary.
-const (
-	EpochLess = iota - 3
-	VerLess
-	RelLess
-	Equal
-	RelGreater
-	VerGreater
-	EpochGreater
-)
+func vercmp(v1, v2 []int) int {
+	for i := len(v1); i < len(v2); i++ {
+		v1 = append(v1, -1)
+	}
+	for i := len(v2); i < len(v1); i++ {
+		v2 = append(v2, -1)
+	}
 
-func pcmp(p1, p2 string) int {
-	n1, err := strconv.ParseUint(p1, 10, 0)
-	an1 := err == nil
-
-	n2, err := strconv.ParseUint(p2, 10, 0)
-	an2 := err == nil
-
-	switch {
-	case an1 && !an2:
-		if (n2 == 0) && (p1[0] != '0') {
-			return VerLess
-		}
-
-		return VerGreater
-
-	case an2 && !an1:
-		if (n1 == 0) && (p2[0] != '0') {
-			return VerLess
-		}
-
-		return VerGreater
-
-	default:
-		switch {
-		case p1 < p2:
-			return VerLess
-		case p1 > p2:
-			return VerGreater
+	for i := 0; i < len(v1); i++ {
+		if vc := v1[i] - v2[i]; vc != 0 {
+			return vc
 		}
 	}
 
-	return Equal
+	return 0
 }
 
-func vercmp(v1, v2 string) int {
-	p1 := strings.Split(v1, ".")
-	p2 := strings.Split(v2, ".")
-
-	for i := len(p1); i < len(p2); i++ {
-		p1 = append(p1, "0")
-	}
-	for i := len(p2); i < len(p1); i++ {
-		p2 = append(p2, "0")
-	}
-
-	for i := len(p1) - 1; i >= 0; i-- {
-		if pc := pcmp(p1[i], p2[i]); pc != 0 {
-			return pc
-		}
-	}
-
-	return Equal
-}
-
-// Vercmp compares two versions. It follows the same rules as Arch
-// Linux's vercmp utility. For more info, see
-// https://www.archlinux.org/pacman/vercmp.8.html
+// Vercmp compares two versions. If the epochs are the same and the
+// semver versions have different lengths, but are equal up until the
+// end of the shorter one, the longer one is considered greater.
 func Vercmp(v1, v2 Version) int {
-	switch {
-	case v1.Epoch < v2.Epoch:
-		return EpochLess
-	case v1.Epoch > v2.Epoch:
-		return EpochGreater
+	if ec := v1.Epoch - v2.Epoch; ec != 0 {
+		return ec
 	}
 
 	if vc := vercmp(v1.Ver, v2.Ver); vc != 0 {
 		return vc
 	}
 
-	switch {
-	case v1.Rel < v2.Rel:
-		return RelLess
-	case v1.Rel > v2.Rel:
-		return RelGreater
-	}
-
-	return Equal
+	return v1.Rel - v2.Rel
 }
--- puck/version_test.go
+++ puck/version_test.go
@@ -8,102 +8,72 @@ import (
 )
 
 func TestVercmp(t *testing.T) {
+	lt := func(out int) bool {
+		return out < 0
+	}
+	//gt := func(out int) bool {
+	//	return out > 0
+	//}
+	eq := func(out int) bool {
+		return out == 0
+	}
+
 	tests := []struct {
 		v1, v2 puck.Version
-		out    int
+		cmp    func(int) bool
 	}{
 		{
-			puck.Version{1, "1.0.0", 0},
-			puck.Version{2, "1.0.0", 0},
-			puck.EpochLess,
+			puck.Version{1, []int{1, 0, 0}, 0},
+			puck.Version{2, []int{1, 0, 0}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0a", 0},
-			puck.Version{0, "1.0b", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1}, 0},
+			puck.Version{0, []int{1, 0}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0b", 0},
-			puck.Version{0, "1.0beta", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1, 0}, 0},
+			puck.Version{0, []int{1, 1}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0beta", 0},
-			puck.Version{0, "1.0p", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1, 1}, 0},
+			puck.Version{0, []int{1, 1, 1}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0p", 0},
-			puck.Version{0, "1.0pre", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1, 1, 1}, 0},
+			puck.Version{0, []int{1, 2}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0pre", 0},
-			puck.Version{0, "1.0rc", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1, 2}, 0},
+			puck.Version{0, []int{2, 0}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0rc", 0},
-			puck.Version{0, "1.0", 0},
-			puck.VerLess,
+			puck.Version{0, []int{2, 0}, 0},
+			puck.Version{0, []int{3, 0, 0}, 0},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0", 0},
-			puck.Version{0, "1.0.a", 0},
-			puck.VerLess,
+			puck.Version{0, []int{1, 0, 0}, 0},
+			puck.Version{0, []int{1, 0, 0}, 1},
+			lt,
 		},
 		{
-			puck.Version{0, "1.0.a", 0},
-			puck.Version{0, "1.0.1", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1", 0},
-			puck.Version{0, "1.0", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1.0", 0},
-			puck.Version{0, "1.1", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1.1", 0},
-			puck.Version{0, "1.1.1", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1.1.1", 0},
-			puck.Version{0, "1.2", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1.2", 0},
-			puck.Version{0, "2.0", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "2.0", 0},
-			puck.Version{0, "3.0.0", 0},
-			puck.VerLess,
-		},
-		{
-			puck.Version{0, "1.0.0", 0},
-			puck.Version{0, "1.0.0", 1},
-			puck.RelLess,
-		},
-		{
-			puck.Version{1, "1.0.0", 5},
-			puck.Version{1, "1.0.0", 5},
-			puck.Equal,
+			puck.Version{1, []int{1, 0, 0}, 5},
+			puck.Version{1, []int{1, 0, 0}, 5},
+			eq,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(fmt.Sprintf("cmp(%v, %v)", test.v1, test.v2), func(t *testing.T) {
 			out := puck.Vercmp(test.v1, test.v2)
-			if out != test.out {
-				t.Fatalf("Expected %v\nGot %v", test.out, out)
+			if !test.cmp(out) {
+				t.Fatalf("Unexpected ouput: %v", out)
 			}
 		})
 	}
